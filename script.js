482b139ca0d9b3881d265138363d7682194667b1
Nathan-design-creator <anyiamchidumebi@gmail.com>
2024-12-29 19:54:57 +0100

    html dom activity completed

---
+++ script.js
@@ -0,0 +1,10 @@
+var heading = document.getElementById("myheading").innerHTML;
+document.write(heading);
+
+function getSurprise() {
+    document.getElementById("surprise").innerHTML = "😀 You are Awesome !!!";
+    document.getElementById("surprise").style.color = "red"
+}
+
+document.body.style.backgroundColor = "seagreen"
+document.body.style.color = "white"
